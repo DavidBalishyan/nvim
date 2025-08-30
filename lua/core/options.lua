4889df4c30cd12434f4022fa81f986c42cdaa1b6
David Balishyan <davidbalishyan12@gmail.com>
2025-08-31 03:33:22 +0400

    splited the init.lua into files to keep a better structure

---
+++ lua/core/options.lua
@@ -0,0 +1,29 @@
+-- Line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Mouse, clipboard, indent, undo
+vim.o.mouse = 'a'
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+vim.o.breakindent = true
+vim.o.undofile = true
+
+-- Searching
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- UI
+vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.inccommand = 'split'
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
